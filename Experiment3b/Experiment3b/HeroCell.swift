32d1422c8069c769d12202f855bb33e2631793e4
Dave Gumba <dave_gumba@Yahoo.com>
2017-06-09 23:20:20 -0400

    Added assets
    ;

---
+++ Experiment3b/Experiment3b/HeroCell.swift
@@ -0,0 +1,8 @@
+
+import UIKit
+
+class HeroCell: UICollectionViewCell {
+    
+    @IBOutlet weak var heroImageView: UIImageView!
+    
+}
---
+++ Experiment3b/Experiment3b/ViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class ViewController: UICollectionViewController {
+    
+    let heroList = [#imageLiteral(resourceName: "hero-ww"), #imageLiteral(resourceName: "hero-socrates"), #imageLiteral(resourceName: "hero-yuno"), #imageLiteral(resourceName: "hero-batman"), #imageLiteral(resourceName: "hero-camus"),
+                    #imageLiteral(resourceName: "hero-gintoki"), #imageLiteral(resourceName: "hero-diogenes"), #imageLiteral(resourceName: "hero-daredevil"), #imageLiteral(resourceName: "hero-saitama"),
+                    #imageLiteral(resourceName: "hero-erza")]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Choose a Hero"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Hero", for: indexPath) as! HeroCell
+        cell.heroImageView.image = heroList[indexPath.row]
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let hero = heroList[indexPath.item]
+        
+        print(hero)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
