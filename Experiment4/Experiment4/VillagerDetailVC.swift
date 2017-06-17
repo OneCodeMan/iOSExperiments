ec5519a3612057e231d560a4f6666f174e9d7e45
Dave Gumba <dave_gumba@Yahoo.com>
2017-06-17 19:52:52 -0400

    Landscape problem

--- Experiment4/Experiment4/VillagerDetailVC.swift
+++ Experiment4/Experiment4/VillagerDetailVC.swift
@@ -4,11 +4,27 @@ import UIKit
 class VillagerDetailVC: UIViewController {
     
     var villager: Villager!
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var villagerImageView: UIImageView!
+    @IBOutlet weak var catchphraseLabel: UILabel!
+    @IBOutlet weak var personalityLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(villager)
+        nameLabel.text = villager.name.capitalized
+        catchphraseLabel.text = "\"\(villager.catchphrase)\""
+        speciesLabel.text = villager.species
+        birthdayLabel.text = villager.birthday
+        villagerImageView.image = UIImage(named: "\(villager.villagerId)")
+        
     }
 
 
