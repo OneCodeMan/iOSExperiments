1bb533b69461463b58a02f551a3414d57d93c18a
Dave Gumba <dave_gumba@Yahoo.com>
2017-06-23 20:12:08 -0400

    added outlets for the checkboxes

--- Experiment10/Experiment10/ChooseShakeViewController.swift
+++ Experiment10/Experiment10/ChooseShakeViewController.swift
@@ -1,13 +1,15 @@
 import UIKit
 
-class ChooseShakeController: UIViewController {
+class ChooseShakeController: UIViewController, BEMCheckBoxDelegate {
+    
+    @IBOutlet weak var sizeSmallCheckbox: BEMCheckBox!
+    @IBOutlet weak var sizeMediumCheckbox: BEMCheckBox!
+    @IBOutlet weak var sizeLargeCheckbox: BEMCheckBox!
     
     @IBOutlet weak var vanillaFlavourLabel: UILabel!
     @IBOutlet weak var vanillaFlavourCheckbox: BEMCheckBox!
-    
     @IBOutlet weak var chocolateFlavourLabel: UILabel!
     @IBOutlet weak var chocolateFlavourCheckbox: BEMCheckBox!
-    
     @IBOutlet weak var strawberryFlavourLabel: UILabel!
     @IBOutlet weak var strawberryFlavourCheckbox: BEMCheckBox!
     
@@ -26,12 +28,32 @@ class ChooseShakeController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sizeSmallCheckbox.delegate = self
+        sizeMediumCheckbox.delegate = self
+        sizeLargeCheckbox.delegate = self
+        
+        vanillaFlavourCheckbox.delegate = self
+        chocolateFlavourCheckbox.delegate = self
+        strawberryFlavourCheckbox.delegate = self
+        dietaryCarbsCheckbox.delegate = self
+        dietaryProteinCheckbox.delegate = self
+        dietarySugarCheckbox.delegate = self
+        dietaryVeganCheckbox.delegate = self
+        
         dietaryCarbsCheckbox.boxType = .square
         dietaryProteinCheckbox.boxType = .square
         dietarySugarCheckbox.boxType = .square
         dietaryVeganCheckbox.boxType = .square
     }
-
+    
+    func didTap(_ checkBox: BEMCheckBox) {
+        print(checkBox.tag)
+    }
+    
+    
+    /*
+     MARK: ORIENTATION LOGIC
+     */
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait
     }
