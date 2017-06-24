493df01c78d4f8b5b525af9767c70f25343fc363
Dave Gumba <dave_gumba@Yahoo.com>
2017-06-23 21:10:45 -0400

    Collected info. ready to pass to other viewcontroller

--- Experiment10/Experiment10/ChooseShakeViewController.swift
+++ Experiment10/Experiment10/ChooseShakeViewController.swift
@@ -5,6 +5,7 @@ class ChooseShakeController: UIViewController, BEMCheckBoxDelegate {
     @IBOutlet weak var sizeSmallCheckbox: BEMCheckBox!
     @IBOutlet weak var sizeMediumCheckbox: BEMCheckBox!
     @IBOutlet weak var sizeLargeCheckbox: BEMCheckBox!
+    var sizeOption: String?
     
     @IBOutlet weak var vanillaFlavourLabel: UILabel!
     @IBOutlet weak var vanillaFlavourCheckbox: BEMCheckBox!
@@ -12,6 +13,7 @@ class ChooseShakeController: UIViewController, BEMCheckBoxDelegate {
     @IBOutlet weak var chocolateFlavourCheckbox: BEMCheckBox!
     @IBOutlet weak var strawberryFlavourLabel: UILabel!
     @IBOutlet weak var strawberryFlavourCheckbox: BEMCheckBox!
+    var flavourOption: String?
     
     @IBOutlet weak var dietaryCarbsCheckbox: BEMCheckBox!
     @IBOutlet weak var dietaryCarbsLabel: UILabel!
@@ -22,11 +24,29 @@ class ChooseShakeController: UIViewController, BEMCheckBoxDelegate {
     @IBOutlet weak var dietaryVeganCheckbox: BEMCheckBox!
     @IBOutlet weak var dietaryVeganLabel: UILabel!
     
+    var sizeCheckBoxes: [BEMCheckBox]?
+    var flavourCheckBoxes: [BEMCheckBox]?
+    var dietaryCheckBoxes: [BEMCheckBox]?
+    
+    let sizes: [String] = ["small", "medium", "large"]
+    let flavours: [String] = ["vanilla", "chocolate", "strawberry"]
+    let dietary: [String] = ["no carbs", "no sugar", "high protein", "vegan"]
+    
     @IBAction func orderButton(_ sender: UIButton) {
+        
+        var selectedChoices: [String] = []
+        for (index, choice) in dietaryCheckBoxes!.enumerated() where choice.on == true {
+            selectedChoices.append(dietary[index])
+        }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sizeCheckBoxes = [sizeSmallCheckbox, sizeMediumCheckbox, sizeLargeCheckbox]
+        flavourCheckBoxes = [vanillaFlavourCheckbox, chocolateFlavourCheckbox, strawberryFlavourCheckbox]
+        dietaryCheckBoxes = [dietaryCarbsCheckbox, dietaryProteinCheckbox, dietarySugarCheckbox, dietaryVeganCheckbox]
         
         sizeSmallCheckbox.delegate = self
         sizeMediumCheckbox.delegate = self
@@ -47,7 +67,40 @@ class ChooseShakeController: UIViewController, BEMCheckBoxDelegate {
     }
     
     func didTap(_ checkBox: BEMCheckBox) {
-        print(checkBox.tag)
+        let tag = checkBox.tag
+        
+        switch tag {
+        case 0...2:
+            toggleSize(of: tag)
+            sizeOption = sizes[tag]
+        case 3...5:
+            toggleFlavour(of: tag)
+            flavourOption = flavours[tag-3]
+        case 6...9:
+           print("Dietary")
+        default:
+            break
+        }
+        
+    }
+    
+    /*
+     MARK: TAP HELPERS
+    */
+    func toggleSize(of sizeTag: Int) {
+        
+        for box in sizeCheckBoxes! where box.tag != sizeTag {
+            box.on = false
+        }
+        
+    }
+    
+    func toggleFlavour(of flavourTag: Int) {
+        
+        for box in flavourCheckBoxes! where box.tag != flavourTag {
+            box.on = false
+        }
+        
     }
     
     
