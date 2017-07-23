189a1d6fcb78ad904bb46ebd37924bc546a8bb34
Dave Gumba <dave_gumba@Yahoo.com>
2017-07-23 17:56:41 -0400

    mr robot done

---
+++ Experiment12/Experiment12/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Experiment12
+//
+//  Created by Dave Gumba on 2017-07-23.
+//  Copyright © 2017 Dave's Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
+
